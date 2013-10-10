fbe5fd8c3daa43f9203b9aa8c7aacb12da8e79ae
hm9408 <Hugomario050894>
2013-10-09 21:15:16 -0500

    Se le agregaron los boundaries al mapa para que no se pueda salir de Bogotá. Se borra el marker sobre el mapa si se toca el mapa de nuevo.

--- src/com/nip/protectblock/MainActivity.java
+++ src/com/nip/protectblock/MainActivity.java
@@ -3,10 +3,11 @@ package com.nip.protectblock;
 import java.util.List;
 
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -19,62 +20,67 @@ import android.widget.Toast;
 
 public class MainActivity extends FragmentActivity {
 
+	private LatLngBounds BOGOTA = new LatLngBounds(new LatLng(4.59354,-74.26964), new LatLng(4.79952,-73.98262));
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-		
-		final GoogleMap map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map))
-	               .getMap();
+
+		final GoogleMap map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+		//para que solo muestre a bogota
+		map.moveCamera( CameraUpdateFactory.newLatLngBounds(BOGOTA, 0) );
 		map.setMyLocationEnabled(true);
 		map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+			Marker marker;
+			@Override
+			public void onMapClick(LatLng point) {
+				if(marker!=null)
+					marker.remove();
+				marker= map.addMarker(new MarkerOptions().position(point)
+						.title(getAddress(point).get(0).getAddressLine(0)).snippet(getAddress(point).get(0).getAddressLine(1)+" - "+getAddress(point).get(0).getAddressLine(2)));
+				marker.showInfoWindow();
+				getAddress(point);
+			}
+		});
 
-	        @Override
-	        public void onMapClick(LatLng point) {
-	            
-	            map.addMarker(new MarkerOptions().position(point)
-	            		.title(getAddress(point).get(0).getAddressLine(0)).snippet(getAddress(point).get(0).getAddressLine(1)+" - "+getAddress(point).get(0).getAddressLine(2))).showInfoWindow();
-	            getAddress(point);
-	        }
-	    });
-		
-		map.addMarker(new MarkerOptions()
-        .position(new LatLng(0, 0))
-        .title("Hello world")
-        .draggable(true));
-        //.icon(BitmapDescriptorFactory.fromAsset("finger-thump-black.jpg")));
-		
-		final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
-		Marker melbourne = map.addMarker(new MarkerOptions()
-		                          .position(MELBOURNE)
-		                          .title("Melbourne")
-		                          .snippet("Population: 4,137,400"));
+//		map.addMarker(new MarkerOptions()
+//		.position(new LatLng(0, 0))
+//		.title("Hello world")
+//		.draggable(true));
+//		.icon(BitmapDescriptorFactory.fromAsset("finger-thump-black.jpg")));
+//		final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+//		Marker melbourne = map.addMarker(new MarkerOptions()
+//		.position(MELBOURNE)
+//		.title("Melbourne")
+//		.snippet("Population: 4,137,400"));
 	}
-	
+
 	public List<Address> getAddress(LatLng point) {
-        try {
-            Geocoder geocoder;
-            List<Address> addresses;
-            geocoder = new Geocoder(this);
-            if (point.latitude != 0 || point.longitude != 0) {
-                addresses = geocoder.getFromLocation(point.latitude ,
-                        point.longitude, 1);
-                        String address = addresses.get(0).getAddressLine(0);
-                        String city = addresses.get(0).getAddressLine(1);
-                        String country = addresses.get(0).getAddressLine(2);
-                        System.out.println(address+" - "+city+" - "+country);
-            return addresses;
-            } else {
-                Toast.makeText(this, "latitude and longitude are null",
-                        Toast.LENGTH_LONG).show();
-                return null;
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+		try {
+			Geocoder geocoder;
+			List<Address> addresses;
+			geocoder = new Geocoder(this);
+			if (point.latitude != 0 || point.longitude != 0) {
+				addresses = geocoder.getFromLocation(point.latitude ,
+						point.longitude, 1);
+				String address = addresses.get(0).getAddressLine(0);
+				String city = addresses.get(0).getAddressLine(1);
+				String country = addresses.get(0).getAddressLine(2);
+				System.out.println(address+" - "+city+" - "+country);
+				return addresses;
+			} else {
+				Toast.makeText(this, "latitude and longitude are null",
+						Toast.LENGTH_LONG).show();
+				return null;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
